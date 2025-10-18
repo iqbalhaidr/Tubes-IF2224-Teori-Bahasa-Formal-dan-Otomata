a7a7296a19c445618915f1e82021cecc973710bc
jhotlann <19623263@std.stei.itb.ac.id>
2025-10-18 08:45:56 +0700

    fix:handle float as int

--- test/test5.pas
+++ test/test5.pas
@@ -1,12 +1,12 @@
 program test5;
 
 var
-    angka: array[1,2,3,4,5] of integer; 
+    angka: array[1..5] of integer;
     i: integer;
 
 begin
     for i := 1 to 5 do
         angka[i] := i;
 
-    angka[3] := 100;
+    angka[3] := 3.1415E+2;
 end.
