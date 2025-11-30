883b230b769eb2fc43902ed8db8d23eb3f48e79d
FerdinandGabe1805 <Mariasilverangela98@gmail.com>
2025-11-30 18:55:26 +0700

    Fix: type undef on string type

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -510,7 +510,8 @@ class SemanticAnalyzer:
     def visit_BuiltinTypeNode(self, node):
         mapping = {
             "integer": TYPE_INTEGER, "real": TYPE_REAL,
-            "boolean": TYPE_BOOLEAN, "char": TYPE_CHAR
+            "boolean": TYPE_BOOLEAN, "char": TYPE_CHAR,
+            "string" : TYPE_STRING
         }
 
         typecode = mapping.get(node.info, TYPE_UNDEFINED)
